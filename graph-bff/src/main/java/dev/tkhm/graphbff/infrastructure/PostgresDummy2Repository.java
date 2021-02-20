6059a7fe310b4020dc419b06e9ef86c4f7239ad3
tkhm <no-reply@tkhm.dev>
2021-02-20 22:03:03 +0900

    refactor: add convert

--- graph-bff/src/main/java/dev/tkhm/graphbff/infrastructure/PostgresDummy2Repository.java
+++ graph-bff/src/main/java/dev/tkhm/graphbff/infrastructure/PostgresDummy2Repository.java
@@ -1,19 +1,18 @@
 package dev.tkhm.graphbff.infrastructure;
 
-import dev.tkhm.graphbff.domain.model.Dummy1;
-import dev.tkhm.graphbff.domain.model.Dummy2;
-import dev.tkhm.graphbff.domain.services.Dummy2Repository;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
-import dev.tkhm.graphbff.infrastructure.dummy2.Dummy2OrmEntity;
-import io.quarkus.hibernate.orm.PersistenceUnit;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.stream.Collectors;
+
+import dev.tkhm.graphbff.domain.model.Dummy2;
+import dev.tkhm.graphbff.domain.services.Dummy2Repository;
+import dev.tkhm.graphbff.infrastructure.dummy2.Dummy2OrmEntity;
+import io.quarkus.hibernate.orm.PersistenceUnit;
 
 @ApplicationScoped
 @Default
@@ -29,9 +28,7 @@ public class PostgresDummy2Repository implements Dummy2Repository {
                     .createQuery("SELECT dummy2 FROM Dummy2OrmEntity dummy2", Dummy2OrmEntity.class);
             Collection<Dummy2> dummyEntities = dummy2Query.getResultList()
                     .stream()
-                    .map(each -> {
-                        return new Dummy2().setId(each.id);
-                    }).collect(Collectors.toList());
+                    .map(each -> each.toModel()).collect(Collectors.toList());
             return dummyEntities;
         } catch (Exception e) {
             e.printStackTrace();
@@ -41,11 +38,9 @@ public class PostgresDummy2Repository implements Dummy2Repository {
 
     @Override
     public void remove(final Dummy2 dummy) {
-
     }
 
     @Override
     public void save(final Dummy2 dummy) {
-
     }
 }
--- graph-bff/src/main/java/dev/tkhm/graphbff/infrastructure/dummy2/Dummy2OrmEntity.java
+++ graph-bff/src/main/java/dev/tkhm/graphbff/infrastructure/dummy2/Dummy2OrmEntity.java
@@ -1,15 +1,23 @@
 package dev.tkhm.graphbff.infrastructure.dummy2;
 
-import dev.tkhm.graphbff.domain.model.Dummy2;
-import io.quarkus.agroal.DataSource;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import dev.tkhm.graphbff.domain.model.Dummy2;
+
 @Entity
 @Table(name = "dummy2")
 public class Dummy2OrmEntity {
+    public Dummy2OrmEntity() {}
+    public Dummy2OrmEntity(Dummy2 dummy2) {
+        this.id = dummy2.getId();
+    }
+
     @Id
     public String id;
+
+    public Dummy2 toModel() {
+        return new Dummy2().setId(this.id);
+    }
 }
