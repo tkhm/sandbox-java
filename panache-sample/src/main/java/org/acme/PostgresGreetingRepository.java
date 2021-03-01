9c5a8609f443b3709331adb6a31666e53693e595
tkhm <no-reply@mail.tkhm.dev>
2021-03-01 15:26:02 +0900

    fix: replace with hibernates

--- panache-sample/src/main/java/org/acme/PostgresGreetingRepository.java
+++ panache-sample/src/main/java/org/acme/PostgresGreetingRepository.java
@@ -6,19 +6,38 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Default;
+import javax.inject.Inject;
 import javax.inject.Qualifier;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @ApplicationScoped
 @IfBuildProfile("prod")
-public class PostgresGreetingRepository implements GreetingRepository, PanacheRepository<GreetingModel> {
+public class PostgresGreetingRepository implements GreetingRepository {
+    @Inject
+    EntityManager em;
+
     @Override
     public Collection<GreetingModel> allGreetings() {
-        return listAll();
+        TypedQuery<GreetingModelDto> fetchQuery = em
+                .createQuery("SELECT greeting FROM GreetingModelDto greeting", GreetingModelDto.class);
+        Collection<GreetingModelDto> greetings = new ArrayList<>(fetchQuery.getResultList());
+        List<GreetingModel> greetingResultList = new ArrayList<>();
+        greetings.forEach(each -> {
+            GreetingModel result = new GreetingModel();
+            result.id = each.id;
+            greetingResultList.add(result);
+        });
+        return greetingResultList;
     }
 
     @Override
     public void save(final GreetingModel greeting) {
-        persist(greeting);
+        GreetingModelDto modelDto = new GreetingModelDto();
+        modelDto.id = greeting.id;
+        em.persist(modelDto);
     }
 }
