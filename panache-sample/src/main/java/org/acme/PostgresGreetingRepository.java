28377bbee5e190c56564cdb52932b44588c62358
tkhm <no-reply@tkhm.dev>
2021-03-01 15:38:43 +0900

    fix: adding save with transactional

--- panache-sample/src/main/java/org/acme/PostgresGreetingRepository.java
+++ panache-sample/src/main/java/org/acme/PostgresGreetingRepository.java
@@ -1,43 +1,33 @@
 package org.acme;
 
 import io.quarkus.arc.profile.IfBuildProfile;
-import io.quarkus.arc.profile.UnlessBuildProfile;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Default;
-import javax.inject.Inject;
-import javax.inject.Qualifier;
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 @ApplicationScoped
 @IfBuildProfile("prod")
-public class PostgresGreetingRepository implements GreetingRepository {
-    @Inject
-    EntityManager em;
-
+public class PostgresGreetingRepository implements GreetingRepository, PanacheRepository<GreetingModelDto> {
     @Override
     public Collection<GreetingModel> allGreetings() {
-        TypedQuery<GreetingModelDto> fetchQuery = em
-                .createQuery("SELECT greeting FROM GreetingModelDto greeting", GreetingModelDto.class);
-        Collection<GreetingModelDto> greetings = new ArrayList<>(fetchQuery.getResultList());
         List<GreetingModel> greetingResultList = new ArrayList<>();
-        greetings.forEach(each -> {
-            GreetingModel result = new GreetingModel();
-            result.id = each.id;
-            greetingResultList.add(result);
-        });
+            listAll().forEach(each -> {
+                GreetingModel result = new GreetingModel();
+                result.id = each.id;
+                greetingResultList.add(result);
+            });
         return greetingResultList;
     }
 
     @Override
+    @Transactional
     public void save(final GreetingModel greeting) {
         GreetingModelDto modelDto = new GreetingModelDto();
         modelDto.id = greeting.id;
-        em.persist(modelDto);
+        persist(modelDto);
     }
 }
